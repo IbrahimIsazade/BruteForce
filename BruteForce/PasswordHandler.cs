0611a4dba82e0004f83505aa94d1e8f17cccbd12
IbrahimIsazade <ibrahimisazade264@gmail.com>
2026-06-03 17:00:58 +0300

    PasswordHandler: Added GenerateTargetPassword logic

--- BruteForce/PasswordHandler.cs
+++ BruteForce/PasswordHandler.cs
@@ -43,12 +43,20 @@ namespace BruteForce
         // Requirement 4b.
         public string GenerateTargetPassword()
         {
-            // TODO:
-            // 1. Create a Random instance.
-            // 2. Pick a length between 4 and 5 (since 6 is exclusive: [4-6) ).
-            // 3. Randomly select characters from a defined character set (e.g., a-z) to build the string.
+            Random random = new Random();
 
-            throw new NotImplementedException("Implement the random password generator here.");
+            int length = random.Next(4, 6);
+
+            const string chars = "abcdefghijklmnopqrstuvwxyz";
+            StringBuilder passwordBuilder = new StringBuilder();
+
+            for (int i = 0; i < length; i++)
+            {
+                int index = random.Next(chars.Length);
+                passwordBuilder.Append(chars[index]);
+            }
+
+            return passwordBuilder.ToString();
         }
     }
 }
