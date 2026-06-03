51d01b7ce83ab87bc704fd52eaa3d5806aadc169
IbrahimIsazade <ibrahimisazade264@gmail.com>
2026-06-03 21:07:35 +0300

    Added PerformanceLogger class

---
+++ BruteForce/PerformanceLogger.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+namespace BruteForce
+{
+    public class PerformanceLogger
+    {
+        private readonly string _logFilePath;
+
+        public PerformanceLogger(string logFilePath = "performance_log.txt")
+        {
+            _logFilePath = logFilePath;
+        }
+
+        // Logs the result of a single attack run (Requirement 8)
+        public void LogRun(string attackType, string foundPassword, TimeSpan duration, long totalAttempts)
+        {
+
+        }
+
+        // Logs a direct comparison between the two methods after both have been run.
+        public void LogComparison(TimeSpan singleThreadDuration, TimeSpan multiThreadDuration)
+        {
+
+        }
+    }
+}
